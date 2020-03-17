ebab79b7e2cb16bbb17102e927d35ac89a2cb60c
mmelcot <mmelcot.professional@gmail.com>
2020-03-17 22:41:12 +0100

    Add .circleci/config.yml

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <div>Hello World</div>
           <p>
             {this.state.wow}
           </p>
